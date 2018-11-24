dee86e806b3fcf51719912a0298c02b24eb9609b
puiiyuen <peiyuan.chen@smu.ca>
2018-11-24 16:54:28 -0400

    v1.1 topological sort detect deadlock with comment

--- manager.cpp
+++ manager.cpp
@@ -1,3 +1,8 @@
+/*
+ * manager.cpp
+ * fork trains and detect deadlock
+ */
+
 //
 // Created by peiyuan on 15/11/18.
 //
@@ -13,8 +18,6 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <signal.h>
 
 #define NORTH 0
@@ -22,9 +25,16 @@
 #define SOUTH 2
 #define EAST 3
 
-#define INFINI 65535
+#define INF 65535
 
 using namespace std;
+
+/*
+ * Set of semaphore
+ * dirLock: semaphore for direction
+ * junction: semaphore for junction
+ * rwMatrix: semaphore for read write matrix.txt
+ */
 
 struct semLock
 {
@@ -33,35 +43,48 @@ struct semLock
     sem_t *rwMatrix;
 };
 
+
+/*
+ * Function: readQueue
+ * read train sequence from sequence.txt
+ * return: vector<char> queue
+ */
+
 vector<char> readQueue()
 {
-    string content="-1";
+    string content = "-1";
 
     fstream file("sequence.txt");
 
-    if(file.is_open()) {
-        getline(file,content);
-        vector<char> queue(content.begin(),content.end());
+    if (file.is_open()) {
+        getline(file, content);
+        vector<char> queue(content.begin(), content.end());
         file.close();
         return queue;
-     } else {
-        vector<char> queue(content.begin(),content.end());
+    } else {
+        vector<char> queue(content.begin(), content.end());
         return queue;
     }
 }
 
+/*
+ * Function: convertQueue
+ * convert char queue to int queue
+ * param: preQueue: The queue needs to be convert
+ * return: vector<int> queue
+ */
+
 vector<int> convertQueue(vector<char> preQueue)
 {
     vector<int> newQueue;
-    for (char i : preQueue)
-    {
-        if(i =='N')
+    for (char i : preQueue) {
+        if (i == 'N')
             newQueue.push_back(NORTH);
-        else if(i =='S')
+        else if (i == 'S')
             newQueue.push_back(SOUTH);
-        else if(i =='W')
+        else if (i == 'W')
             newQueue.push_back(WEST);
-        else if(i =='E')
+        else if (i == 'E')
             newQueue.push_back(EAST);
         else
             newQueue.push_back(-1);
@@ -69,14 +92,19 @@ vector<int> convertQueue(vector<char> preQueue)
     return newQueue;
 }
 
+/*
+ * Function: initMatrix
+ * initialize matrix.txt base on the number of train
+ * param: numOfTrain: number of train
+ * return: true: Successfully initialize false: initialize failed
+ */
+
 bool initMatrix(int numOfTrain)
 {
     fstream file("matrix.txt");
-    if(file.is_open())
-    {
-        for (int i=0;i<numOfTrain;i++)
-        {
-            file<<"0 0 0 0"<<endl;
+    if (file.is_open()) {
+        for (int i = 0; i < numOfTrain; i++) {
+            file << "0 0 0 0" << endl;
         }
         file.close();
         return true;
@@ -84,22 +112,34 @@ bool initMatrix(int numOfTrain)
     return false;
 }
 
+/*
+ * Function: initialize named semaphore
+ * param: sLock: set of semaphore
+ * return: true: Successfully initialize false: initialize failed
+ */
+
 bool initSemaphore(semLock *&sLock)
 {
-    sLock->dirLock[NORTH]=sem_open("north",O_CREAT,0777,1);
-    sLock->dirLock[SOUTH]=sem_open("south",O_CREAT,0777,1);
-    sLock->dirLock[WEST]=sem_open("west",O_CREAT,0777,1);
-    sLock->dirLock[EAST]=sem_open("east",O_CREAT,0777,1);
-    sLock->rwMatrix=sem_open("rwMatrix",O_CREAT,0777,1);
-    sLock->junction=sem_open("junction",O_CREAT,0777,1);
+    sLock->dirLock[NORTH] = sem_open("north", O_CREAT, 0777, 1);
+    sLock->dirLock[SOUTH] = sem_open("south", O_CREAT, 0777, 1);
+    sLock->dirLock[WEST] = sem_open("west", O_CREAT, 0777, 1);
+    sLock->dirLock[EAST] = sem_open("east", O_CREAT, 0777, 1);
+    sLock->rwMatrix = sem_open("rwMatrix", O_CREAT, 0777, 1);
+    sLock->junction = sem_open("junction", O_CREAT, 0777, 1);
 
     return !(sLock->junction == SEM_FAILED ||
-             sLock->rwMatrix==SEM_FAILED ||
-             sLock->dirLock[NORTH]==SEM_FAILED ||
-             sLock->dirLock[SOUTH]==SEM_FAILED ||
-             sLock->dirLock[WEST]==SEM_FAILED ||
-             sLock->dirLock[EAST]==SEM_FAILED);
+             sLock->rwMatrix == SEM_FAILED ||
+             sLock->dirLock[NORTH] == SEM_FAILED ||
+             sLock->dirLock[SOUTH] == SEM_FAILED ||
+             sLock->dirLock[WEST] == SEM_FAILED ||
+             sLock->dirLock[EAST] == SEM_FAILED);
 }
+
+/*
+ * Function: close the connection of named semaphore and delete semaphores
+ * param: sLock: set of semaphore
+ * return: true: Successfully close and delete
+ */
 
 bool unlinkCloseSem(semLock *&sLock)
 {
@@ -121,28 +161,38 @@ bool unlinkCloseSem(semLock *&sLock)
 
 }
 
+/*
+ * function: getRandom
+ * get random double number. range:(0.2-0.7)
+ * return: double random number
+ */
+
 double getRandom()
 {
-    return (double)rand()/RAND_MAX;
+    return (double) rand() / RAND_MAX;
 }
 
-int readMatrix(int i,int j)
+/*
+ * function: readMatrix
+ * read the matrix[i][j] from matrix.txt
+ * param: i: row index j: column index
+ * return: the content of matrix[i][j]
+ * if cannot read the matrix[i][j] return -1;
+ */
+
+int readMatrix(int i, int j)
 {
-    int m=0,n=0;
+    int m = 0, n = 0;
     string line;
     string temp;
 
     fstream file("matrix.txt");
-    if(file.is_open())
-    {
-        while(getline(file,line)&&m<=i)
-        {
-            if(m==i)
-            {
+    if (file.is_open()) {
+        while (getline(file, line) && m <= i) {
+            if (m == i) {
                 istringstream myNum(line);
-                while(getline(myNum,temp,' ')){
-                    if(n==j)
-                    {
+                while (getline(myNum, temp, ' ')) {
+                    if (n == j) {
                         file.close();
                         return atoi(temp.c_str());
                     }
@@ -159,22 +209,34 @@ int readMatrix(int i,int j)
     return -1;
 }
 
-bool checkDeadlock(semLock *&sLock,int **&matrix,int numOfTrain,pid_t childPid[])
+/*
+ * function: checkDeadlock
+ * check the deadlock from train
+ * param: sLock: set of semaphore
+ *        matrix: new matrix for detect deadlock
+ *        numOfTrain: number of train
+ *        childPid: store pid of child process
+ * return: true: if there is deadlock false: no deadlock
+ */
+
+bool checkDeadlock(semLock *&sLock, int **&matrix, int numOfTrain, pid_t childPid[])
 {
     sem_wait(sLock->rwMatrix);
 
-    int count[numOfTrain+4];
-    int stack[numOfTrain+4],top=-1;
-    int numOfNoInDegree=0;
+    int count[numOfTrain + 4];//count in-degree of each node
+    int stack[numOfTrain + 4], top = -1;
+    int numOfNoInDegree = 0;
     vector<int> deadLockTrain;
 
-    for(int j=0;j<numOfTrain+4;j++)
-    {
-        count[j]=0;
+
+    //topological sort check if there is a circle
+    for (int j = 0; j < numOfTrain + 4; j++) {
+        count[j] = 0;
     }
-    for(int i=0;i<numOfTrain;i++)
-    {
-        for(int j=0;j<4;j++) {
+
+    //convert matrix.txt file to deadlock detect matrix
+    for (int i = 0; i < numOfTrain; i++) {
+        for (int j = 0; j < 4; j++) {
             if (readMatrix(i, j) == 2) {
                 matrix[numOfTrain + j][i] = 1;
                 count[i]++;
@@ -186,111 +248,100 @@ bool checkDeadlock(semLock *&sLock,int **&matrix,int numOfTrain,pid_t childPid[]
         }
     }
 
-    for(int k=0;k<numOfTrain+4;k++)
-    {
-        if(count[k]==0)
-        {
+    //push node with 0 in-degree
+    for (int k = 0; k < numOfTrain + 4; k++) {
+        if (count[k] == 0) {
             top++;
-            stack[top]=k;
+            stack[top] = k;
         }
     }
 
-    while(top>-1)
-    {
-        int t=stack[top];
+
+    //delete node with 0 in-degree and all edges started from this node
+    while (top > -1) {
+        int t = stack[top];
         top--;
         numOfNoInDegree++;
-        for(int i=0;i<numOfTrain+4;i++)
-        {
-            if(matrix[t][i]==1)
-            {
+        for (int i = 0; i < numOfTrain + 4; i++) {
+            if (matrix[t][i] == 1) {
                 count[i]--;
-                matrix[t][i]=0;
-                if(count[i]==0)
-                {
+                matrix[t][i] = 0;
+                if (count[i] == 0) {
                     top++;
-                    stack[top]=i;
+                    stack[top] = i;
                 }
             }
 
         }
     }
 
-    if(numOfNoInDegree==numOfTrain+4)
-    {
+    //check if there are nodes that they still stay in the deadlock detect matrix
+    if (numOfNoInDegree == numOfTrain + 4) {
         sem_post(sLock->rwMatrix);
         return false;
-    }
-    else
-    {
-        cout<<"\n---There is deadlock---"<<endl;
-        int start=-1,end=-1,tempStart;
+    } else {
 
-        for(int i=0;i<numOfTrain+4;i++)
-        {
-            if(count[i]>0)
-            {
-                start=i;
+        //print the deadlock circle
+        cout << "\n---There is deadlock---" << endl;
+        int start = -1, end = -1, tempStart;
+
+        for (int i = 0; i < numOfTrain + 4; i++) {
+            if (count[i] > 0) {
+                start = i;
                 break;
             }
         }
 
-        tempStart=start;
+        tempStart = start;
 
-        while(start!=end)
-        {
-            for(int j=0;j<numOfTrain+4;j++)
-            {
-                if(matrix[tempStart][j]==1)
-                {
-                    if(tempStart<numOfTrain)
-                        cout<<"Train<"<<tempStart<<"> --> ";
-                    else
-                    {
-                        if(tempStart-numOfTrain==NORTH)
-                            cout<<"North-Lock --> ";
-                        else if(tempStart-numOfTrain==SOUTH)
-                            cout<<"South-Lock --> ";
-                        else if(tempStart-numOfTrain==WEST)
-                            cout<<"West-Lock -->";
-                        else if(tempStart-numOfTrain==EAST)
-                            cout<<"East-Lock --> ";
+        while (start != end) {
+            for (int j = 0; j < numOfTrain + 4; j++) {
+                if (matrix[tempStart][j] == 1) {
+                    if (tempStart < numOfTrain)
+                        cout << "Train<" << tempStart << "> --> ";
+                    else {
+                        if (tempStart - numOfTrain == NORTH)
+                            cout << "North-Lock --> ";
+                        else if (tempStart - numOfTrain == SOUTH)
+                            cout << "South-Lock --> ";
+                        else if (tempStart - numOfTrain == WEST)
+                            cout << "West-Lock -->";
+                        else if (tempStart - numOfTrain == EAST)
+                            cout << "East-Lock --> ";
                         else
-                            cout<<"Wrong item";
+                            cout << "Wrong item";
                     }
-                    tempStart=j;
-                    end=j;
+                    tempStart = j;
+                    end = j;
                     break;
                 }
             }
         }
 
-        if(start<numOfTrain)
-            cout<<"Train <"<<start<<">"<<endl;
-        else
-        {
-            if(start-numOfTrain==NORTH)
-                cout<<"North-Lock"<<endl;
-            else if(start-numOfTrain==SOUTH)
-                cout<<"South-Lock"<<endl;
-            else if(start-numOfTrain==WEST)
-                cout<<"West-Lock"<<endl;
-            else if(start-numOfTrain==EAST)
-                cout<<"East-Lock"<<endl;
+        if (start < numOfTrain)
+            cout << "Train <" << start << ">" << endl;
+        else {
+            if (start - numOfTrain == NORTH)
+                cout << "North-Lock" << endl;
+            else if (start - numOfTrain == SOUTH)
+                cout << "South-Lock" << endl;
+            else if (start - numOfTrain == WEST)
+                cout << "West-Lock" << endl;
+            else if (start - numOfTrain == EAST)
+                cout << "East-Lock" << endl;
             else
-                cout<<"Wrong item"<<endl;
+                cout << "Wrong item" << endl;
         }
 
-        cout<<endl;
+        cout << endl;
 
-        for (int i=0;i<numOfTrain;i++) {
-            if(childPid[i]!=-999)
-            {
-                kill(childPid[i],SIGTERM);
-                cout<<"Train <"<< i <<"> is killed (pid: "<<childPid[i]<<")"<<endl;
+        //kill child processes when deadlock
+        for (int i = 0; i < numOfTrain; i++) {
+            if (childPid[i] != -999) {
+                kill(childPid[i], SIGTERM);
+                cout << "Train <" << i << "> is killed (pid: " << childPid[i] << ")" << endl;
             }
         }
-
 
         sem_post(sLock->rwMatrix);
         return true;
@@ -298,106 +349,100 @@ bool checkDeadlock(semLock *&sLock,int **&matrix,int numOfTrain,pid_t childPid[]
 
 }
 
+/*
+ * function: manager
+ * manage trains, semaphore, initialize values
+ *
+ */
+
 void manager()
 {
-    vector<int> trainQueue=convertQueue(readQueue());
 
-    semLock *lock=(semLock *)malloc(sizeof(semLock));
+    //read the sequence and convert
+    vector<int> trainQueue = convertQueue(readQueue());
 
-    unlinkCloseSem(lock);
-
-    int numOfTrain=trainQueue.size();
-
+    int numOfTrain = trainQueue.size();
+    int **deadLockMatrix;
     pid_t childPid[numOfTrain];
 
-    for(int c=0;c<numOfTrain;c++)
-    {
-        childPid[numOfTrain]=-999;
-    }
+    //initialize semaphores
+    semLock *lock = (semLock *) malloc(sizeof(semLock));
 
-    int**deadLockMatrix;
+    //clear semaphores
+    unlinkCloseSem(lock);
 
-    deadLockMatrix=(int **)malloc((4+numOfTrain)*sizeof(int *));
-
-    for(int i=0;i<numOfTrain+4;i++)
-    {
-        deadLockMatrix[i]=(int *)malloc((4+numOfTrain)*sizeof(int *));
-        for(int j=0;j<numOfTrain+4;j++)
-        {
-            if(i==j)
-                deadLockMatrix[i][j]=0;
+    //initial deadlock matrix
+    deadLockMatrix = (int **) malloc((4 + numOfTrain) * sizeof(int *));
+    for (int i = 0; i < numOfTrain + 4; i++) {
+        deadLockMatrix[i] = (int *) malloc((4 + numOfTrain) * sizeof(int *));
+        for (int j = 0; j < numOfTrain + 4; j++) {
+            if (i == j)
+                deadLockMatrix[i][j] = 0;
             else
-                deadLockMatrix[i][j]=INFINI;
+                deadLockMatrix[i][j] = INF;
         }
     }
 
-    if(!trainQueue.empty()&&initMatrix(numOfTrain)&&initSemaphore(lock))
-    {
-        double p=0;
-        while(p<0.2||p>0.7)
-        {
-            cout<<"Input a number (0.2-0.7) for check deadlock or create new train process:";
-            cin>>p;
+    //initial array to save pid of child process
+    for (int c = 0; c < numOfTrain; c++) {
+        childPid[numOfTrain] = -999;
+    }
+
+    //initialize matrix.txt and semaphore
+    if (!trainQueue.empty() && initMatrix(numOfTrain) && initSemaphore(lock)) {
+        double p = 0;
+        while (p < 0.2 || p > 0.7) {
+            cout << "Input a number (0.2-0.7) for check deadlock or create new train process:";
+            cin >> p;
         }
 
         int intTrainNo = 0;
-        char *trainNO = (char *)malloc(sizeof(int));
-        char *trainDir= (char *)malloc(sizeof(int));
+        char *trainNO = (char *) malloc(sizeof(int));
+        char *trainDir = (char *) malloc(sizeof(int));
 
-        while(true)
-        {
-            if(getRandom()<p)
-            {
-                if(checkDeadlock(lock,deadLockMatrix,numOfTrain,childPid))
-                {
-                    cout<<"\nChild processes terminated"<<endl;
-                    cout<<"Exiting Manager..."<<endl;
+        while (true) {
+            if (getRandom() < p) {
+
+                //check deadlock
+                if (checkDeadlock(lock, deadLockMatrix, numOfTrain, childPid)) {
+                    cout << "\nChild processes terminated" << endl;
+                    cout << "Exiting Manager..." << endl;
                     unlinkCloseSem(lock);
                     exit(0);
-                }
-                else
-                {
-                    if(trainQueue.empty())
-                    {
+                } else {
+                    //no deadlock check if all trains passed
+                    if (trainQueue.empty()) {
                         sem_wait(lock->rwMatrix);
-                        bool free= true;
+                        bool free = true;
                         for (int i = 0; i < numOfTrain; ++i) {
                             for (int j = 0; j < 4; ++j) {
-                                if(readMatrix(i,j)!=0)
-                                {
-                                    free=false;
+                                if (readMatrix(i, j) != 0) {
+                                    free = false;
                                     break;
                                 }
                             }
                         }
-                        if(free)
-                        {
-                            cout<<"\n---All trains have passed junction---"<<endl;
+                        if (free) {
+                            cout << "\n---All trains have passed junction---" << endl;
                             sem_post(lock->rwMatrix);
                             unlinkCloseSem(lock);
                             exit(0);
-                        }
-                        else
-                        {
+                        } else {
                             sem_post(lock->rwMatrix);
                         }
                     }
                 }
-            }
-            else
-            {
-                if(!trainQueue.empty())
-                {
-                    sprintf(trainNO,"%d",intTrainNo);
-                    sprintf(trainDir,"%d",trainQueue[0]);
-                    childPid[intTrainNo]=fork();
-                    if(childPid[intTrainNo]==0)
-                    {
-                        execlp("./train",trainNO,trainDir,NULL);
+            } else {
+
+                //fork child process execute trains
+                if (!trainQueue.empty()) {
+                    sprintf(trainNO, "%d", intTrainNo);
+                    sprintf(trainDir, "%d", trainQueue[0]);
+                    childPid[intTrainNo] = fork();
+                    if (childPid[intTrainNo] == 0) {
+                        execlp("./train", trainNO, trainDir, NULL);
                         exit(0);
-                    }
-                    else
-                    {
+                    } else {
                         trainQueue.erase(trainQueue.begin());
                         intTrainNo++;
                     }
@@ -405,19 +450,27 @@ void manager()
             }
 
         }
-    }
-    else
-    {
+    } else {
+        //initialize failed
         unlinkCloseSem(lock);
-        cout<<"Initialize Failed..."<<endl;
-        cout<<"Program terminated..."<<endl;
+        cout << "Initialize Failed..." << endl;
+        cout << "Program terminated..." << endl;
         exit(0);
     }
 }
 
+
+/*
+ * main function
+ */
+
 int main()
 {
-    srand((unsigned)time(nullptr));
+
+    //random seed
+    srand((unsigned) time(nullptr));
+
     manager();
 
+    return 0;
 }
--- train.cpp
+++ train.cpp
@@ -19,40 +19,47 @@
 #define SOUTH 2
 #define EAST 3
 
-struct semLock
-{
+using namespace std;
+
+/*
+ * Set of semaphore
+ * dirLock: semaphore for direction
+ * junction: semaphore for junction
+ * rwMatrix: semaphore for read write matrix.txt
+ */
+
+struct semLock {
     sem_t *dirLock[4];
     sem_t *junction;
     sem_t *rwMatrix;
 };
 
-using namespace std;
+/*
+ * function: writeMatrix
+ * write matrix.txt
+ * param: i: row index, j: column index, signal: content to write
+ */
 
-void writeMatrix(int i,int j,int signal)
-{
-    int m=0;
+void writeMatrix(int i, int j, int signal) {
+    int m = 0;
 
     string line;
     string temp;
 
     char sig[1];
-    sprintf(sig,"%d", signal);
+    sprintf(sig, "%d", signal);
 
     fstream file("matrix.txt");
-    if(file.is_open())
-    {
-        while(getline(file,line)&&m<=i)
-        {
+    if (file.is_open()) {
+        while (getline(file, line) && m <= i) {
             long linePos = file.tellp();
-            if(m==i)
-            {
-                long pos=linePos-2*(3-j)-2;
+            if (m == i) {
+                long pos = linePos - 2 * (3 - j) - 2;
                 file.seekp(pos);
-                file.write(sig,1);
+                file.write(sig, 1);
 
                 break;
-            }
-            else
+            } else
                 m++;
         }
 
@@ -60,105 +67,113 @@ void writeMatrix(int i,int j,int signal)
     }
 }
 
-void train(int id,int dir,semLock *&lock)
-{
-    string direction,rightDir;
-    if(dir==NORTH)
-    {
-        direction="North";
-        rightDir="West";
-    }
-    else if(dir==SOUTH)
-    {
-        direction="South";
-        rightDir="East";
-    }
-    else if(dir==WEST)
-    {
-        direction="West";
-        rightDir="South";
-    }
-    else if(dir==EAST)
-    {
-        direction="East";
-        rightDir="North";
-    }
-    else
-    {
-        direction="Wrong Direction";
-        rightDir="Wrong Direction";
+/*
+ * function: train
+ * trains pass the junction
+ * param: id: train id , dir: direction of the train, lock: set of semaphore
+ */
+
+void train(int id, int dir, semLock *&lock) {
+
+    //set direction for print out
+    string direction, rightDir;
+    if (dir == NORTH) {
+        direction = "North";
+        rightDir = "West";
+    } else if (dir == SOUTH) {
+        direction = "South";
+        rightDir = "East";
+    } else if (dir == WEST) {
+        direction = "West";
+        rightDir = "South";
+    } else if (dir == EAST) {
+        direction = "East";
+        rightDir = "North";
+    } else {
+        direction = "Wrong Direction";
+        rightDir = "Wrong Direction";
     }
 
     //Train started
-    cout<<"---Train <"<<id<<"> "<<direction<<" train started---"<<endl;
+    cout << "---Train <" << id << "> " << direction << " train started---" << endl;
 
     //Train requests for lock
     sem_wait(lock->rwMatrix);
-    writeMatrix(id,dir,1);
-    cout<<"---Train <"<<id<<"> Requests for "<<direction<<"-Lock---"<<endl;
+    writeMatrix(id, dir, 1);
+    cout << "---Train <" << id << "> Requests for " << direction << "-Lock---" << endl;
     sem_post(lock->rwMatrix);
 
     //Train acquires lock
     sem_wait(lock->dirLock[dir]);
     sem_wait(lock->rwMatrix);
-    writeMatrix(id,dir,2);
-    cout<<"---Train <"<<id<<"> Acquires "<<direction<<"-Lock---"<<endl;
+    writeMatrix(id, dir, 2);
+    cout << "---Train <" << id << "> Acquires " << direction << "-Lock---" << endl;
     sem_post(lock->rwMatrix);
 
     //Train request for right direction
     sem_wait(lock->rwMatrix);
-    writeMatrix(id,(dir+1)%4,1);
-    cout<<"---Train <"<<id<<"> Requests for "<<rightDir<<"-Lock---"<<endl;
+    writeMatrix(id, (dir + 1) % 4, 1);
+    cout << "---Train <" << id << "> Requests for " << rightDir << "-Lock---" << endl;
     sem_post(lock->rwMatrix);
 
     //Train acquires right direction lock
-    sem_wait(lock->dirLock[(dir+1)%4]);
+    sem_wait(lock->dirLock[(dir + 1) % 4]);
     sem_wait(lock->rwMatrix);
-    writeMatrix(id,(dir+1)%4,2);
-    cout<<"---Train <"<<id<<"> Acquires "<<rightDir<<"-Lock---"<<endl;
+    writeMatrix(id, (dir + 1) % 4, 2);
+    cout << "---Train <" << id << "> Acquires " << rightDir << "-Lock---" << endl;
     sem_post(lock->rwMatrix);
 
     //Train passing junction
-    cout<<"---Train <"<<id<<"> Requests for Junction-Lock---"<<endl;
+    cout << "---Train <" << id << "> Requests for Junction-Lock---" << endl;
     sem_wait(lock->junction);
-    cout<<"---Train <"<<id<<"> Acquires Junction-Lock---"<<endl;
-    cout<<"---Train <"<<id<<"> is passing junction---"<<endl;
+    cout << "---Train <" << id << "> Acquires Junction-Lock---" << endl;
+    cout << "---Train <" << id << "> is passing junction---" << endl;
     sleep(2);
     sem_post(lock->junction);
-    cout<<"---Train <"<<id<<"> Releases Junction-Lock---"<<endl;
+    cout << "---Train <" << id << "> Releases Junction-Lock---" << endl;
 
     //Train releases locks
     sem_wait(lock->rwMatrix);
     sem_post(lock->dirLock[dir]);
-    writeMatrix(id,dir,0);
-    cout<<"---Train <"<<id<<"> Releases "<<direction<<"-Lock---"<<endl;
-    sem_post(lock->dirLock[(dir+1)%4]);
-    writeMatrix(id,(dir+1)%4,0);
-    cout<<"---Train <"<<id<<"> Releases "<<rightDir<<"-Lock---"<<endl;
+    writeMatrix(id, dir, 0);
+    cout << "---Train <" << id << "> Releases " << direction << "-Lock---" << endl;
+    sem_post(lock->dirLock[(dir + 1) % 4]);
+    writeMatrix(id, (dir + 1) % 4, 0);
+    cout << "---Train <" << id << "> Releases " << rightDir << "-Lock---" << endl;
     sem_post(lock->rwMatrix);
 
 
 }
 
-bool initSemaphore(semLock *&sLock)
-{
-    sLock->dirLock[NORTH]=sem_open("north",O_CREAT,0777,1);
-    sLock->dirLock[SOUTH]=sem_open("south",O_CREAT,0777,1);
-    sLock->dirLock[WEST]=sem_open("west",O_CREAT,0777,1);
-    sLock->dirLock[EAST]=sem_open("east",O_CREAT,0777,1);
-    sLock->rwMatrix=sem_open("rwMatrix",O_CREAT,0777,1);
-    sLock->junction=sem_open("junction",O_CREAT,0777,1);
+/*
+ * Function: initialize named semaphore
+ * param: sLock: set of semaphore
+ * return: true: Successfully initialize false: initialize failed
+ */
+
+bool initSemaphore(semLock *&sLock) {
+    sLock->dirLock[NORTH] = sem_open("north", O_CREAT, 0777, 1);
+    sLock->dirLock[SOUTH] = sem_open("south", O_CREAT, 0777, 1);
+    sLock->dirLock[WEST] = sem_open("west", O_CREAT, 0777, 1);
+    sLock->dirLock[EAST] = sem_open("east", O_CREAT, 0777, 1);
+    sLock->rwMatrix = sem_open("rwMatrix", O_CREAT, 0777, 1);
+    sLock->junction = sem_open("junction", O_CREAT, 0777, 1);
 
     return !(sLock->junction == SEM_FAILED ||
-    sLock->rwMatrix==SEM_FAILED ||
-    sLock->dirLock[NORTH]==SEM_FAILED ||
-    sLock->dirLock[SOUTH]==SEM_FAILED ||
-    sLock->dirLock[WEST]==SEM_FAILED ||
-    sLock->dirLock[EAST]==SEM_FAILED);
+             sLock->rwMatrix == SEM_FAILED ||
+             sLock->dirLock[NORTH] == SEM_FAILED ||
+             sLock->dirLock[SOUTH] == SEM_FAILED ||
+             sLock->dirLock[WEST] == SEM_FAILED ||
+             sLock->dirLock[EAST] == SEM_FAILED);
 }
 
-bool closeSem(semLock *&sLock)
-{
+/*
+ * Function: close the connection of named semaphore
+ * param: sLock: set of semaphore
+ * return: true: Successfully close
+ */
+
+bool closeSem(semLock *&sLock) {
     sem_close(sLock->junction);
     sem_close(sLock->rwMatrix);
     sem_close(sLock->dirLock[EAST]);
@@ -170,20 +185,23 @@ bool closeSem(semLock *&sLock)
 
 }
 
-int main(int argc, char **argv)
-{
-    semLock *lock=(semLock *)malloc(sizeof(semLock));
-    
-    int trainId=atoi(argv[1]);
-    int direction=atoi(argv[2]);
+/*
+ * main function
+ */
 
-    if(initSemaphore(lock))
-    {
-        train(trainId,direction,lock);
+int main(int argc, char **argv) {
+
+    semLock *lock = (semLock *) malloc(sizeof(semLock));
+
+    int trainId = atoi(argv[0]);
+    int direction = atoi(argv[1]);
+
+    if (initSemaphore(lock)) {
+        train(trainId, direction, lock);
         closeSem(lock);
     }
 
-    cout<<"---Train <"<<trainId<<"> Finished passing---"<<endl;
+    cout << "---Train <" << trainId << "> Finished passing---" << endl;
 
     return 0;
 }
